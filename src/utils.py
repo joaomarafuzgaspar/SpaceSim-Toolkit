Updated `utils.py` Fixed a bug in the MATLAB script related to plotting average absolute deviations.

src/utils.py
@@ -1,5 +1,7 @@
+import os
 import numpy as np
 import matlab.engine
+
 
 def rmse(X_est, X_true):
     return np.sqrt(
@@ -8,7 +10,20 @@ def rmse(X_est, X_true):
 
 
 def run_visualizer():
-    # Run MATLAB
+    # Start MATLAB engine
     eng = matlab.engine.start_matlab()
-    eng.visualizer(nargout=0)
+
+    # Get the absolute path to the directory containing visualizer.m
+    matlab_functions_path = os.path.abspath("../SpaceSim-Toolkit/src")
+
+    # Add the directory to the MATLAB path
+    eng.addpath(matlab_functions_path, nargout=0)
+
+    # Call the visualizer script
+    try:
+        eng.visualizer(nargout=0)
+    except matlab.engine.MatlabExecutionError as e:
+        print(f"An error occurred: {e}")
+
+    # Quit the MATLAB engine
     eng.quit()